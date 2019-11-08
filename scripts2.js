7d20aa60760919db0340f3d1cbe38de187604813
Sharon Lee <sharonlee714@gmail.com>
2019-11-08 11:51:51 -0800

    add alternate scripts and html file with a checkbox function for 2+ topping option.

---
+++ scripts2.js
@@ -0,0 +1,44 @@
+//business logic
+function Pizza(inputSize, getToppings, prices) {
+  this.size = inputSize;
+  this.toppings = getToppings;
+  this.price = prices;
+}
+
+
+
+// Pizza.prototype.getPrice = function () {
+//   if (this.size === 1 && this.toppings === 1) {
+//     this.price = 10;
+//   } else if (this.size === 1 && this.toppings === 2) {
+//     this.price = 12;
+//   } else if (this.size === 1 && this.toppings === 3) {
+//     this.price = 12;
+//   } else if (this.size === 2 && this.toppings === 1) {
+//     this.price = 12;
+//   } else if (this.size === 2 && this.toppings === 2) {
+//     this.price = 14;
+//   } else {                // } else if (this.size === 2 &&& this.toppings === 3) {
+//     this.price = 14;
+//   }
+// }
+
+//user logic
+$(document).ready(function() {
+  $("form#user").submit(function(event) {
+    event.preventDefault();
+    var getToppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var inputTopping = $(this).val();
+      getToppings.push(inputTopping);
+    });
+    console.log(getToppings);
+    var inputSize = parseInt($("#size").val());
+    var prices= 0;
+    var iWantPizza = new Pizza(inputSize, getToppings, prices);
+    iWantPizza.getPrice();
+    // console.log(iWantPizza.getPrice());
+    $("#results").show();
+    $(".output").append(iWantPizza.price);
+  });
+});
